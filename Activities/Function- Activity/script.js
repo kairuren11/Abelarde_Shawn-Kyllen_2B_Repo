c6e607afbed34e1350c594501f552f8b48277ac5
unknown <shawna@filamer.edu.ph>
2025-03-23 20:48:51 -0700

    activity

---
+++ Activities/Function- Activity/script.js
@@ -0,0 +1,14 @@
+function whatShallWear(temp){
+    if (temp < 60){
+        console.log("Wear a Jacket");
+    }
+    else if (temp < 70) {
+        console.log("Wear a Sweater");
+    }
+    else {
+        console.log("Wear a T-Shirt");
+    }
+}
+whatShallWear(50);
+whatShallWear(60);
+whatShallWear(80);
